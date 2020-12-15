3b37b2927fbac73dff9504d081a7523e6b28ef95
nikolayski <nikolayski@abv.bg>
2020-12-15 11:01:06 +0200

    Refactoring code

--- src/Data/ApplicationDbContext.cs
+++ src/Data/ApplicationDbContext.cs
@@ -12,15 +12,25 @@ namespace Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Cart> Carts { get; set; }
+
         public DbSet<Image> Images { get; set; }
+
         public DbSet<Product> Products { get; set; }
+
         public DbSet<Room> Rooms { get; set; }
+
         public DbSet<UserRoom> UserRooms { get; set; }
+
         public DbSet<RoomImages> RoomImages { get; set; }
+
         public DbSet<Comment> Comments { get; set; }
+
         public DbSet<CartProduct> CartProducts { get; set; }
+
         public DbSet<Recipe> Recipes { get; set; }
+
         public DbSet<Post> Posts { get; set; }
+
         public DbSet<Contact> Contacts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
